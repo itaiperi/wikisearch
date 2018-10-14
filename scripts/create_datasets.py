5bceddbbd793d63e4497c3b5e068036908e806cf
noodlez04 <noodlez040@gmail.com>
2018-10-14 15:33:46 +0300

    Add log prints to dataset creation script

--- scripts/create_datasets.py
+++ scripts/create_datasets.py
@@ -1,7 +1,10 @@
 import argparse
 import os
 import random
+import time
+from collections import defaultdict
 
+import numpy as np
 import pandas as pd
 
 from wikisearch.consts.mongo import WIKI_LANG
@@ -42,6 +45,7 @@ if __name__ == '__main__':
     parser.add_argument('-s', '--seed', help="Random seed", type=int)
     parser.add_argument('-o', '--out', help="Output dir path", required=True)
     parser.add_argument('-d', '--max_distance', type=int, default=20)
+    parser.add_argument('--log-interval', default=100, type=int)
     args = parser.parse_args()
 
     if args.max_distance < 1:
@@ -52,7 +56,11 @@ if __name__ == '__main__':
     graph = WikiGraph(WIKI_LANG)
     graph_keys = sorted(graph.keys())
 
+    entire_start = time.time()
+    distances = defaultdict(list)
+    runtimes = {}
     for dataset_type, num_records in zip(['train', 'validation', 'test'], args.num_records):
+        dataset_start = time.time()
         dataset = []
         for i in range(num_records):
             dest = None
@@ -63,7 +71,12 @@ if __name__ == '__main__':
                 source = rnd_generator.choice(graph_keys)
                 dest, distance = find_at_distance(graph, graph.get_node(source), desired_distance)
             # print("%s\t%s\t%d\t%d" % (source, dest.title, distance, desired_distance))
+            distances[dataset_type].append(distance)
             dataset.append((source, dest.title, distance))
+            if (not i % args.log_interval) and i:
+                print(f"{dataset_type.capitalize()}: {i} datapoints created,"
+                      f" total elapsed time: {time.time() - entire_start:.1f} seconds...")
+        print(f"{dataset_type.capitalize()}: {num_records} datapoints created.")
 
         # Create dataframe from dataset
         df = pd.DataFrame.from_records(dataset, columns=['source', 'destination', 'min_distance'])
@@ -71,5 +84,16 @@ if __name__ == '__main__':
         dataset_path = os.path.abspath(os.path.join(args.out, dataset_type + '.csv'))
         # Save dataset (through dataframe)
         df.to_csv(dataset_path, header=True, index=False, sep='\t')
+        runtimes[dataset_type] = time.time() - dataset_start
 
-    print("Dataset created at", os.path.abspath(args.out))
+    for dataset_type, num_records in zip(['train', 'validation', 'test'], args.num_records):
+        print(f"##### Dataset {dataset_type} #####")
+        print(f"Number of entries: {num_records}\t"
+              f"Build time: {runtimes[dataset_type]:.1f} seconds\t"
+              f"Average build time/entry: {runtimes[dataset_type] / num_records:.2f} seconds")
+        print(f"Min distance: {np.min(distances[dataset_type])}\t"
+              f"Max distance: {np.max(distances[dataset_type])}\t"
+              f"Average distance: {np.mean(distances[dataset_type])}\t"
+              f"Standard Deviation: {np.std(distances[dataset_type])}")
+
+    print("Total elapsed time for all datsets: {:.1f} seconds.".format(time.time() - entire_start))
