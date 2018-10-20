562bfef3b9cd1ff258289dfbe32e472d26eeb868
noodlez04 <noodlez040@gmail.com>
2018-10-20 18:18:21 +0300

    Bugfix in NN architecture

--- wikisearch/heuristics/nn_archs.py
+++ wikisearch/heuristics/nn_archs.py
@@ -6,12 +6,14 @@ import torch.nn.functional as F
 class EmbeddingsDistance(nn.Module):
     def __init__(self, embed_dim):
         super(EmbeddingsDistance, self).__init__()
-        self.siamese_fc1 = nn.Linear(embed_dim, 128)
+        siamese_fc1_size = 128
+        self.siamese_fc1 = nn.Linear(embed_dim, siamese_fc1_size)
         self.siamese_conv1 = nn.Conv1d(1, 32, kernel_size=5)
         self.siamese_conv2 = nn.Conv1d(32, 16, kernel_size=3)
         self.conv1 = nn.Conv1d(32, 16, kernel_size=3)
         self.conv2 = nn.Conv1d(16, 1, kernel_size=1)
-        self.fc1 = nn.Linear(14, 1)
+        # conv5 -> pool2 -> conv3 -> pool2 -> conv3 -> pool
+        self.fc1 = nn.Linear((((((siamese_fc1_size - 4) / 2) - 2) / 2) - 2) / 2, 1)
 
     def forward(self, x1, x2):
         x1, x2 = self.siamese_fc1(x1), self.siamese_fc1(x2)
