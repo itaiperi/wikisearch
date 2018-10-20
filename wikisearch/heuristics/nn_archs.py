af5c8af45f7a4660af09bd8233e4e2511dd46368
noodlez04 <noodlez040@gmail.com>
2018-10-20 14:27:39 +0300

    Change distance model to partially-siamese network

--- wikisearch/heuristics/nn_archs.py
+++ wikisearch/heuristics/nn_archs.py
@@ -1,3 +1,4 @@
+import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
@@ -5,17 +6,20 @@ import torch.nn.functional as F
 class EmbeddingsDistance(nn.Module):
     def __init__(self, embed_dim):
         super(EmbeddingsDistance, self).__init__()
-        self.fc1 = nn.Linear(embed_dim, 128)
-        self.conv1 = nn.Conv1d(2, 32, kernel_size=5)
-        self.conv2 = nn.Conv1d(32, 16, kernel_size=3)
-        self.conv3 = nn.Conv1d(16, 1, kernel_size=1)
-        self.fc3 = nn.Linear(15, 1)
+        self.siamese_fc1 = nn.Linear(embed_dim, 128)
+        self.siamese_conv1 = nn.Conv1d(1, 32, kernel_size=5)
+        self.siamese_conv2 = nn.Conv1d(32, 16, kernel_size=3)
+        self.conv1 = nn.Conv1d(32, 16, kernel_size=3)
+        self.conv2 = nn.Conv1d(16, 1, kernel_size=1)
+        self.fc1 = nn.Linear(14, 1)
 
-    def forward(self, x):
-        x = self.fc1(x)
-        x = F.max_pool1d(self.conv1(x), 2)
-        x = F.max_pool1d(self.conv2(x), 2)
-        x = F.max_pool1d(self.conv3(x), 2)
-        x = self.fc3(x).squeeze(2)
+    def forward(self, x1, x2):
+        x1, x2 = self.siamese_fc1(x1), self.siamese_fc1(x2)
+        x1, x2 = F.relu(F.max_pool1d(self.siamese_conv1(x1), 2)), F.relu(F.max_pool1d(self.siamese_conv1(x2), 2))
+        x1, x2 = F.relu(F.max_pool1d(self.siamese_conv2(x1), 2)), F.relu(F.max_pool1d(self.siamese_conv2(x2), 2))
+        x = torch.stack([x1, x2], dim=1)
+        x = F.relu(F.max_pool1d(self.conv1(x), 2))
+        x = F.relu(F.max_pool1d(self.conv2(x), 2))
+        x = self.fc1(x).squeeze(2)
 
         return x
