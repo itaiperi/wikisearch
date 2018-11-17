a95713a823b50416d8072b5c0c9668653ef2184f
Daniella Pick <danielapick@gmail.com>
2018-11-17 12:11:44 +0200

    Returned the datasets' cleaning to its original method

--- wikisearch/embeddings/embedding.py
+++ wikisearch/embeddings/embedding.py
@@ -26,7 +26,7 @@ class Embedding(metaclass=ABCMeta):
         page = self._mongo_handler.get_page(WIKI_LANG, EMBEDDINGS, title)
         if page:
             vector = page.get(self.__class__.__name__.lower())
-            return Embedding._decode_vector(vector) if vector else None
+            return self._decode_vector(vector) if vector else None
 
     def _store(self, page_id, title, vector):
         """
@@ -35,7 +35,7 @@ class Embedding(metaclass=ABCMeta):
         :param title: The titles to keep its embedding
         :param vector: The title's embedding
         """
-        page = {'_id': page_id, 'title': title, self.__class__.__name__.lower(): Embedding._encode_vector(vector),
+        page = {'_id': page_id, 'title': title, self.__class__.__name__.lower(): self._encode_vector(vector),
                 'last_modified': datetime.datetime.now().__str__()}
         self._mongo_handler.update_page(WIKI_LANG, EMBEDDINGS, page)
 
