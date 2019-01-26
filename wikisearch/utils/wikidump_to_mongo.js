66836b314903c871101f22f6b31eeab323512c3b
noodlez04 <noodlez040@gmail.com>
2019-01-26 16:07:25 +0200

    Bugfixes in wikidump script

--- wikisearch/utils/wikidump_to_mongo.js
+++ wikisearch/utils/wikidump_to_mongo.js
@@ -2,12 +2,10 @@ const dumpster = require('dumpster-dive');
 
 options = {
 	file: process.argv[2],
-//	file: '/home/itai/Downloads/simplewiki_united-states_test.xml',
-	db: 'simplewiki2',
+	db: 'simplewiki',
 	skip_redirects: false,
 	skip_disambig: true,
 	batch_size: 1000,
-	workers: 1,
 	custom: function(doc) {
 	    function capitalizeEnglish(word) {
 	        letter = word[0]
@@ -17,24 +15,15 @@ options = {
                 return word;
             }
         }
-//        function getMethods(obj) {
-//            var result = [];
-//            for (var id in obj) {
-//                try {
-//                    if (typeof(obj[id]) == "function") {
-//                        result.push(id + ": " + obj[id].toString());
-//                    }
-//                } catch (err) {
-//                    result.push(id + ": inaccessible");
-//                }
-//            }
-//            return result;
-//        }
 
         if(doc.isRedirect()) {
-            return {
-                title: doc.title(),
-                redirectTo: doc.redirectTo(),
+            if(doc.redirectTo() == undefined) {
+                throw "Category redirect - Ignoring";
+            } else {
+                return {
+                    title: doc.title(),
+                    redirectTo: doc.redirectTo()['page'],
+                }
             }
         }
 
