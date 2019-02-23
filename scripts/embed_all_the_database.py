a720ad566b214e601f24ffb8fa7e75de2373a5c3
Daniella Pick <danielapick@gmail.com>
2019-02-23 14:05:47 +0200

    Change the embedding script to insert all the embeddings at once

--- scripts/embed_all_the_database.py
+++ scripts/embed_all_the_database.py
@@ -9,37 +9,34 @@ from wikisearch.embeddings import AVAILABLE_EMBEDDINGS
 from wikisearch.utils.mongo_handler import MongoHandler
 
 
-def get_update_embedding_request(page_pr):
-    page_title = page_pr[ENTRY_TITLE]
-
-    embedded_vector = embedder._load_embedding(page_title)
-    if embedded_vector is None:
-        embedded_vector = embedder._embed(page_pr)
-
-    page_id = page_pr[ENTRY_ID]
-
-    return embedder._mongo_handler_embeddings.update_page_request(
-        {'_id': page_id, 'title': page_title,
-         embedder.__class__.__name__.lower(): embedder._encode_vector(embedded_vector),
-         'last_modified': datetime.datetime.now().__str__()})
+def get_page_embedding(embedder, page_pr):
+    embedded_vector = embedder._embed(page_pr)
+    return (embedder.__class__.__name__.lower(), embedder._encode_vector(embedded_vector))
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--embedding", required=True, choices=AVAILABLE_EMBEDDINGS)
+    parser.add_argument("--embeddings", required=True, choices=AVAILABLE_EMBEDDINGS, nargs="+")
     args = parser.parse_args()
 
-    embedder = load_embedder_by_name(args.embedding)
+    embedders = []
+    for embedder in args.embeddings:
+        embedders.append(load_embedder_by_name(embedder))
     mongo_handler_pages = MongoHandler(WIKI_LANG, PAGES)
 
     pages = mongo_handler_pages.get_all_documents()
     len_pages = pages.count()
     start = time.time()
-    update_embeddings_requests = []
+    embedded_vectors = []
     for idx, page in enumerate(pages, 1):
-        update_embeddings_requests.append(get_update_embedding_request(page))
+        page_value = {'_id': page[ENTRY_ID], 'title': page[ENTRY_TITLE],
+                      'last_modified': datetime.datetime.now().__str__()}
+        for embedder in embedders:
+            embedder_class, embedder_value = get_page_embedding(embedder, page)
+            page_value[embedder_class] = embedder_value
+
+        embedded_vectors.append(page_value)
+
         print_progress_bar(idx, len_pages, time.time() - start, prefix=f'Embedding', length=50)
 
-    embedder._mongo_handler_embeddings.bulk_write(update_embeddings_requests)
-
-    print(f"-TIME- Took to embed all the database: {time.time()-start}")
+    embedder._mongo_handler_embeddings.insert_data(embedded_vectors)
