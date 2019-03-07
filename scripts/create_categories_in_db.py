93ed3d9acdfedd95c8ab9a41a35548b0b7cc63da
noodlez04 <noodlez040@gmail.com>
2019-03-07 11:01:05 +0200

    Add resolution to categories creation script

--- scripts/create_categories_in_db.py
+++ scripts/create_categories_in_db.py
@@ -25,15 +25,14 @@ def categories_pages_coverage(categories, pages_categories_list):
     return count, uncovered_pages
 
 
-def choose_categories(pages, percentage):
+def choose_categories(pages, percentage, resolution):
     # Take only pages which are not redirects, and that actually have categories
     pages_with_categories = [set(page[ENTRY_CATEGORIES]) for page in pages
                              if (ENTRY_REDIRECT_TO not in page and len(page[ENTRY_CATEGORIES]))]
     num_pages = len(pages_with_categories)
     categories_counter = Counter([category for categories in pages_with_categories for category in categories])
-    total_categories = sum(categories_counter.values())
 
-    covered_pages_counter, i = 0, 0
+    covered_pages_counter = 0
     # taken_categories is used to see how many categories are required for full coverage, not only required percentage
     taken_categories = set()
     # chosen_categories is the set only of the categories required to cover percentage of the pages
@@ -44,33 +43,33 @@ def choose_categories(pages, percentage):
     uncovered_pages = pages_with_categories
     start_time = time.time()
     while covered_pages_counter < num_pages and len(categories_counter):
-        max_category = categories_counter.most_common(1)[0][0]
-        del categories_counter[max_category]
-        taken_categories.add(max_category)
+        top_categories = [category[0] for category in categories_counter.most_common(resolution)]
+        for category in top_categories:
+            del categories_counter[category]
+        taken_categories.update(top_categories)
         added_covered_pages, uncovered_pages = categories_pages_coverage(taken_categories, uncovered_pages)
         covered_pages_counter += added_covered_pages
         if covered_pages_counter / num_pages >= percentage and chosen_categories is None:
             chosen_categories = taken_categories.copy()
             exact_coverage = covered_pages_counter / num_pages
-        i += 1
-        if i % REPORT_RESOLUTION == 0:
+        if len(taken_categories) % REPORT_RESOLUTION == 0:
             coverage_percentages.append(covered_pages_counter / num_pages)
         print_progress_bar(covered_pages_counter, num_pages, time.time() - start_time,
                            length=50, prefix="Categories Choosing")
-    if i % REPORT_RESOLUTION != 0:
+    if len(taken_categories) % REPORT_RESOLUTION != 0:
         coverage_percentages.append(covered_pages_counter / num_pages)
 
     print(
-        f"Chose {len(chosen_categories)} categories covering {exact_coverage * 100:.1f}% of the DB")
+        f"Chose {len(chosen_categories)} categories covering {exact_coverage * 100:.2f}% of the DB")
     return chosen_categories, coverage_percentages, len(taken_categories)
 
 
-def main(percentage):
+def main(percentage, resolution):
     pages_mongo = MongoHandler(WIKI_LANG, PAGES)
     categories_mongo = MongoHandler(WIKI_LANG, CATEGORIES)
 
     all_pages = list(pages_mongo.get_all_documents())
-    chosen_categories, coverage_percentages_history, total_categories = choose_categories(all_pages, percentage)
+    chosen_categories, coverage_percentages_history, total_categories = choose_categories(all_pages, percentage, resolution)
 
     plt.figure()
     indices = list(range(0, len(coverage_percentages_history) * REPORT_RESOLUTION, REPORT_RESOLUTION)) + [total_categories]
@@ -87,6 +86,9 @@ def main(percentage):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('-p', '--percentage', required=True, type=float, help="Percentage of pages to cover with categories")
+    parser.add_argument('-r', '--resolution', default=1, type=int, help="Number of categories to add in each iteration")
     args = parser.parse_args()
+    if args.percentage > 1.0:
+        raise ValueError(f"Percentage must be in range 0 <= p <= 1.0, got {args.percentage}")
 
-    main(args.percentage)
+    main(args.percentage, args.resolution)
